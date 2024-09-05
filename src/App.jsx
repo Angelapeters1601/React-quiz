297fb4493342173f4386a272a86c981b5d0267c0
Angelapeters1601 <nwattaha@gmail.com>
2024-09-05 20:19:04 +0200

    managed state, passed dispatch as props and displayed components on UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,55 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect, useReducer } from "react";
 import "./App.css";
 import Header from "./Header";
 import MainBody from "./MainBody";
+import Loader from "./Loader";
+import Error from "./Error";
+import StartScreen from "./StartScreen";
+import Question from "./Question";
+
+const initialState = {
+  questions: [],
+  //   "loading", "error","ready","active","finished"
+  status: "loading",
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "dataRecieved":
+      return { ...state, questions: action.payload, status: "ready" };
+    case "dataFailed":
+      return { ...state, status: "error" };
+    case "start":
+      return { ...state, status: "active" };
+    default:
+      throw newError("Action unknown");
+  }
+};
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const { questions, status } = state;
+
+  const numQuestions = questions.length;
+
   useEffect(() => {
     fetch("http://localhost:8000/questions")
       .then((res) => res.json())
-      .then((data) => console.log("Data:", data))
-      .catch((error) => console.error("Error:", error));
+      .then((data) => dispatch({ type: "dataRecieved", payload: data }))
+      .catch((error) => dispatch({ type: "dataFailed", payload: error }));
   }, []);
 
   return (
     <div className="app">
       <Header />
       <MainBody>
-        <p>1/15</p>
-        <p>question?</p>
+        {status === "loading" && <Loader />}
+        {status === "error" && <Error />}
+        {status === "ready" && (
+          <StartScreen numQuestions={numQuestions} dispatch={dispatch} />
+        )}
+        {status === "active" && <Question />}
       </MainBody>
     </div>
   );
